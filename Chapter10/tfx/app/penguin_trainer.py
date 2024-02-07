6ae96f6baab10898d8255796cf483f46852cbfac
Rod Anami <rod.anami@br.ibm.com>
2024-02-07 11:57:10 -0300

    removed 0.22 version

--- Chapter10/tfx/app/penguin_trainer.py
+++ Chapter10/tfx/app/penguin_trainer.py
@@ -1,7 +1,7 @@
 from typing import List
 from absl import logging
 import tensorflow as tf
-import tfx as tfx
+from tfx import v1 as tfx
 from tensorflow import keras
 from tensorflow_transform.tf_metadata import schema_utils
 
--- Chapter10/tfx/app/tfx_pipeline.py
+++ Chapter10/tfx/app/tfx_pipeline.py
@@ -1,71 +1,74 @@
 import tensorflow as tf
-import tfx as tfx
-import os
+from tfx import v1 as tfx
 import urllib.request
 import tempfile
+import os
+import tensorflow_data_validation as tfdv
+import tensorflow_transform as tft
+from tfx.components import CsvExampleGen, StatisticsGen, SchemaGen, ExampleValidator, Transform, Trainer, Evaluator
+from tfx.proto import example_gen_pb2, trainer_pb2, evaluator_pb2
+from tfx.orchestration.experimental.interactive.interactive_context import InteractiveContext
 
 print('TensorFlow version: {}'.format(tf.__version__))
 print('TFX version: {}'.format(tfx.__version__))
 
 PIPELINE_NAME = "penguin-simple"
-
 # Output directory to store artifacts generated from the pipeline.
 PIPELINE_ROOT = os.path.join('pipelines', PIPELINE_NAME)
-# Path to a SQLite DB file to use as an MLMD storage.
-METADATA_PATH = os.path.join('metadata', PIPELINE_NAME, 'metadata.db')
 # Output directory where created models from the pipeline will be exported.
 SERVING_MODEL_DIR = os.path.join('serving_model', PIPELINE_NAME)
 
-from absl import logging
-logging.set_verbosity(logging.INFO)  # Set default logging level.
-
+# Retrive sample dataset and set the path to it
 DATA_ROOT = tempfile.mkdtemp(prefix='tfx-data')  # Create a temporary directory.
-_data_url = 'https://raw.githubusercontent.com/tensorflow/tfx/master/tfx/examples/penguin/data/labelled/penguins_processed.csv'
-_data_filepath = os.path.join(DATA_ROOT, "data.csv")
-urllib.request.urlretrieve(_data_url, _data_filepath)
+data_url = 'https://raw.githubusercontent.com/tensorflow/tfx/master/tfx/examples/penguin/data/labelled/penguins_processed.csv'
+data_filepath = os.path.join(DATA_ROOT, "data.csv")
+urllib.request.urlretrieve(data_url, data_filepath)
 
-_trainer_module_file = 'penguin_trainer.py'
+with open(data_filepath) as input_file:
+    head = [next(input_file) for _ in range(20)]
+print(head)
 
-def _create_pipeline(pipeline_name: str, pipeline_root: str, data_root: str,
-                     module_file: str, serving_model_dir: str,
-                     metadata_path: str) -> tfx.dsl.Pipeline:
-  """Creates a three component penguin pipeline with TFX."""
-  # Brings data into the pipeline.
-  example_gen = tfx.components.CsvExampleGen(input_base=data_root)
+# Create a CsvExampleGen component
+example_gen = tfx.components.CsvExampleGen(input_base=data_filepath)
 
-  # Uses user-provided Python function that trains a model.
-  trainer = tfx.components.Trainer(
-      module_file=module_file,
-      examples=example_gen.outputs['examples'],
-      train_args=tfx.proto.TrainArgs(num_steps=100),
-      eval_args=tfx.proto.EvalArgs(num_steps=5))
+# Create a StatisticsGen component
+statistics_gen = tfx.components.StatisticsGen(examples=example_gen.outputs['examples'])
 
-  # Pushes the model to a filesystem destination.
-  pusher = tfx.components.Pusher(
-      model=trainer.outputs['model'],
-      push_destination=tfx.proto.PushDestination(
-          filesystem=tfx.proto.PushDestination.Filesystem(
-              base_directory=serving_model_dir)))
+# Create a SchemaGen component
+schema_gen = tfx.components.SchemaGen(statistics=statistics_gen.outputs['statistics'])
 
-  # Following three components will be included in the pipeline.
-  components = [
-      example_gen,
-      trainer,
-      pusher,
-  ]
+# Create an ExampleValidator component
+example_validator = ExampleValidator(
+    statistics=statistics_gen.outputs['statistics'],
+    schema=schema_gen.outputs['schema'])
 
-  return tfx.dsl.Pipeline(
-      pipeline_name=pipeline_name,
-      pipeline_root=pipeline_root,
-      metadata_connection_config=tfx.orchestration.metadata
-      .sqlite_metadata_connection_config(metadata_path),
-      components=components)
+# Create a Trainer component
+trainer = tfx.components.Trainer(
+    module_file='penguim_trainer.py',
+    schema=schema_gen.outputs['schema'],
+    examples=example_gen.outputs['examples'],
+    train_args=trainer_pb2.TrainArgs(num_steps=100),
+    eval_args=trainer_pb2.EvalArgs(num_steps=50))
 
-tfx.orchestration.LocalDagRunner().run(
-  _create_pipeline(
-      pipeline_name=PIPELINE_NAME,
-      pipeline_root=PIPELINE_ROOT,
-      data_root=DATA_ROOT,
-      module_file=_trainer_module_file,
-      serving_model_dir=SERVING_MODEL_DIR,
-      metadata_path=METADATA_PATH))
+# Creates a Pusher component
+# Pushes the model to a filesystem destination.
+pusher = tfx.components.Pusher(
+    model=trainer.outputs['model'],
+    push_destination=tfx.proto.PushDestination(
+        filesystem=tfx.proto.PushDestination.Filesystem(
+            base_directory=SERVING_MODEL_DIR)))
+
+# Add all the components to the pipeline
+components = [ example_gen, 
+              statistics_gen, 
+              schema_gen, 
+              example_validator, 
+              trainer, 
+              pusher ]
+
+pipeline = tfx.orchestration.pipeline.Pipeline(
+    pipeline_name=PIPELINE_NAME,
+    pipeline_root=PIPELINE_ROOT,
+    components=components)
+
+tfx.orchestration.LocalDagRunner().run(pipeline)
