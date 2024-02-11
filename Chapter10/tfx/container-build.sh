422509fad85743e403f8087e1e42821c4ccb8b74
Rod Anami <rod.anami@br.ibm.com>
2024-02-11 17:10:22 -0300

    added env variables

--- Chapter10/tfx/container-build.sh
+++ Chapter10/tfx/container-build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Build the container image
+CONTAINER_ORG="rod4n4m1"
+CONTAINER_NAME="tfx-pipeline"
+CONTAINER_VERSION="0.2.0"
 
 if command -v podman &> /dev/null; then
-    podman build -t tfx-pipeline .
+    podman build -t $CONTAINER_ORG/$CONTAINER_NAME:$CONTAINER_VERSION .
 elif command -v docker &> /dev/null; then
-    docker build -t tfx-pipeline .
+    docker build -t $CONTAINER_ORG/$CONTAINER_NAME:$CONTAINER_VERSION .
 else
   echo "Podman or Docker is required. Please install either Podman or Docker."
   exit 1
