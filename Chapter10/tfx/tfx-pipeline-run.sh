e552913f788ed0de443e5987d16b04430f0b2cce
Rod Anami <rod.anami@br.ibm.com>
2024-02-11 16:35:48 -0300

    updated run script

--- Chapter10/tfx/tfx-pipeline-run.sh
+++ Chapter10/tfx/tfx-pipeline-run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 LOG_FILE="tfx-pipeline.log"
+CONTAINER_NAME="tfx-pipeline-$(date +%Y%m%d%H%M)"
 
 if command -v podman &> /dev/null; then
     echo "Pipeline is running. Check the logs with 'tail -f $LOG_FILE'"
-    podman run --name tfx-pipeline -d rod4n4m1/tfx-pipeline:latest > $LOG_FILE
+    podman run --name $CONTAINER_NAME -it rod4n4m1/tfx-pipeline:latest > $LOG_FILE
 elif command -v docker &> /dev/null; then
     echo "Pipeline is running. Check the logs with 'tail -f $LOG_FILE'"
-    docker run --name tfx-pipeline -d rod4n4m1/tfx-pipeline:latest > $LOG_FILE
+    docker run --name $CONTAINER_NAME -it rod4n4m1/tfx-pipeline:latest > $LOG_FILE
 else
   echo "Podman or Docker is required. Please install either Podman or Docker."
   exit 1
