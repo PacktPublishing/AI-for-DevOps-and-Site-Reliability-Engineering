d3c0b86d54fe5e8694db75420e1de2de610d8fef
Rod Anami <rod.anami@kyndryl.com>
2024-07-06 09:05:22 -0300

    improved documentation of scripts Signed-off-by: Rod Anami <rod.anami@kyndryl.com>

--- Chapter13/azure-ml/azure-setup.sh
+++ Chapter13/azure-ml/azure-setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+# Based on the following documentation:
+# https://learn.microsoft.com/en-us/azure/machine-learning/how-to-configure-cli
 
+USER_NAME=$1
+SUBSCRIPTION_ID=$2
 WS_NAME="sre-ml-workspace"
 RG_NAME="sre-ml-rg"
 LOCATION="eastus"
 
 # Sign in to Azure
-az login
+az login -u $USER_NAME
+# Set the default subscription
+az account set -s $SUBSCRIPTION_ID
 
 # Install Azure Machine Learning extension
 az extension add -n azure-cli-ml
